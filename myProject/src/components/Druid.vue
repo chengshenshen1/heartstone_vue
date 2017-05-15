18d418cfd41602e4a72b126cff706030dbc23928
chengshenshen1 <535653568@qq.com>
2017-05-15 22:06:48 +0800

    数据添加进来（图片）

--- myProject/src/components/Druid.vue
+++ myProject/src/components/Druid.vue
@@ -1,10 +1,14 @@
 <template>
 <div class="druid">
   <div class="picture">
-    图片存放区
+    <ul class="cardList">
+      <li v-for="val in cardUrl"><img :src="val" alt="""></li>
+    </ul>
   </div>
   <div class="picked">
-     已选卡片区
+     <ul>
+       <li></li>
+     </ul>
   </div>
 </div>
 </template>
@@ -14,12 +18,66 @@
     name: 'druid',
     data () {
       return {
-        msg: '我是德鲁伊'
+        cardUrl: [],
+        pickedCard: {}
+      }
+    },
+    mounted: function () {
+      this.getUrl()
+    },
+    methods: {
+      getUrl () {
+        this.$http.get('../static/cards.json').then((res) => {
+          for (let i = 0; i < res.data.length; i++) {
+            if (res.data[i].playerClass === ('NEUTRAL' || 'Druid')) {
+              let by = function (name) {
+                return function (o, p) {
+                  let a, b
+                  if (typeof o === 'object' && typeof p === 'object' && o && p) {
+                    a = o[name]
+                    b = p[name]
+                    if (a === b) { return 0 }
+                    if (typeof a === typeof b) { return a < b ? -1 : 1 }
+                    return typeof a < typeof b ? -1 : 1
+                  }
+                }
+              }
+              let sortedCard = res.data.sort(by(res.data[i].cost))
+              this.cardUrl.push(sortedCard[i].img)
+            }
+          }
+        })
       }
     }
   }
 </script>
 
 <style>
+  .druid{
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+  }
+  .picture{
+    box-sizing: border-box;
+    width: 75%;
 
+  }
+  .picture .cardList{
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: 0;
+    padding:0;
+  }
+  .picture .cardList li{
+    width: 33.33%;
+    list-style: none;
+  }
+  .picture .cardList li img{
+    width: 200px;
+  }
+  .picked{
+    box-sizing: border-box;
+    width: 25%;
+  }
 </style>
